acaab3ad1c276a5ec0585f882b747994c32790ea
levi <levimolnardev@gmail.com>
2024-04-17 16:40:02 +0200

    Added temp model, improved text spacing, added complex gradient background.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import './App.css';
-import { useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { AmbientLight, Mesh } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+import "./App.css";
+import { useRef } from "react";
+
 
 const ProductView = () => {
   const meshRef = useRef<Mesh>(null!);
 
+  const parcelPath = new URL("1982_sony_betacam.glb", import.meta.url);
+  const model = useLoader(GLTFLoader, parcelPath.href);
+  console.log(model);
+
   useFrame((state, delta) => {
-    meshRef.current.rotation.x = .25*-state.mouse.y + .1*Math.PI;
-    meshRef.current.rotation.y = .25*state.mouse.x + .25*Math.PI;
+    meshRef.current.rotation.x = .25*-state.pointer.y + .1*Math.PI;
+    // meshRef.current.rotation.y = .25*state.mouse.x + .25*Math.PI;
+    meshRef.current.rotation.y = .25*state.pointer.x + 1.15*Math.PI;
   });
 
   return (
-    <mesh 
-      ref={meshRef} 
-      position={[0, .25, 0]}
-      rotation={[.1*Math.PI, .25*Math.PI, 0]}
+    <primitive 
+      object={model.scene} 
+      ref={meshRef}
+      position={[-2.5,2.5,-35]}
+      rotation={[.1*Math.PI, 1.15*Math.PI, 0]}
     >
-      <boxGeometry />
-      <meshBasicMaterial color={"#777"} />
-    </mesh>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]}/>
+      {/* <meshBasicMaterial color={"#777"} /> */}
+    </primitive>
+    // <mesh 
+    //   ref={meshRef} 
+    //   position={[0, .25, 0]}
+    //   rotation={[.1*Math.PI, .25*Math.PI, 0]}
+    // >
+    //   <boxGeometry />
+    //   <meshBasicMaterial color={"#777"} />
+    // </mesh>
   )
 }
 
@@ -38,8 +56,8 @@ const App = () => {
         </div>
       </div>
       <Canvas 
-        camera={{ fov: 30 }}
-        // style={{ background: "#ff00ff55" }}
+        camera={{ fov: 50 }}
+        style={{ width: "1000px", height: "1000px" }}
       >
         <ProductView />
       </Canvas>
