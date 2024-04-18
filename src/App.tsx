89b5ad70aa7c758e24bf22f73bb3a3b934dca5fc
levi <levimolnardev@gmail.com>
2024-04-18 03:53:17 +0200

    Added mouseover animation with easing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,55 @@
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { AmbientLight, Mesh } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Mesh } from "three";
+
+// import { useSpring, animated } from '@react-spring/three'
+import { easing } from 'maath'
 
 import "./App.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
+const ProductView = ({reset}: {reset: boolean}) => {
 
-const ProductView = () => {
-  const meshRef = useRef<Mesh>(null!);
+  const ref = useRef<Mesh>(null!);
+  
+  // const { rotation } = useSpring({
+  //   rotation: 
+  //     reset || !ref.current
+  //     ? [.1*Math.PI, 1.25*Math.PI, 0] 
+  //     : [ref.current.rotation.x, ref.current.rotation.y, ref.current.rotation.z] 
+  // }) as any;
 
   const parcelPath = new URL("1982_sony_betacam.glb", import.meta.url);
   const model = useLoader(GLTFLoader, parcelPath.href);
-  console.log(model);
+
+  // useFrame((state) => {
+  //   if (!reset) {
+  //     ref.current.rotation.x = .2*-state.pointer.y + .1*Math.PI;
+  //     ref.current.rotation.y = .2*state.pointer.x + 1.25*Math.PI;
+  //   };
+  // });
 
   useFrame((state, delta) => {
-    meshRef.current.rotation.x = .1*-state.pointer.y + .1*Math.PI;
-    // meshRef.current.rotation.y = .25*state.mouse.x + .25*Math.PI;
-    meshRef.current.rotation.y = .1*state.pointer.x + 1.25*Math.PI;
+    easing.dampE(ref.current.rotation, [.2*-state.pointer.y + .1*Math.PI, .2*state.pointer.x + 1.25*Math.PI, 0, 0], .2, delta);
   });
 
   return (
-    <primitive 
-      object={model.scene} 
-      ref={meshRef}
+    <mesh
+      ref={ref}
       position={[-2.5, 1, -35]}
-      rotation={[.1*Math.PI, 1.15*Math.PI, 0]}
+      rotation={[.1*Math.PI, 1.25*Math.PI, 0]}
     >
-      {/* <ambientLight intensity={50}/> */}
-      <ambientLight intensity={.4}/>
+      <primitive object={model.scene} />
+      <ambientLight intensity={.4} />
       <pointLight position={[-5, 10, -50]} intensity={5_000} color="#80ffff" />
-    </primitive>
-    // <mesh 
-    //   ref={meshRef} 
-    //   position={[0, .25, 0]}
-    //   rotation={[.1*Math.PI, .25*Math.PI, 0]}
-    // >
-    //   <boxGeometry />
-    //   <meshBasicMaterial color={"#777"} />
-    // </mesh>
+    </mesh>
   )
 }
 
 const App = () => {
+
+  // const [resetRotation, setResetRotation] = useState(true);
+
   return (
     <div className="wrap">
       <div className="label large">
@@ -49,8 +57,8 @@ const App = () => {
         <span className="thin">SHOWCASE</span>
       </div>
       <div className="productinfo">
-        <div className="bold medium">A-100</div>
-        <div className="bold medium">CAMCORDER</div>
+        <div className="boldest medium">SONY</div>
+        <div className="bold medium">BETACAM 1982</div>
         <div className="thin small">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
         </div>
@@ -58,8 +66,10 @@ const App = () => {
       <Canvas 
         camera={{ fov: 50 }}
         style={{ width: "1000px", height: "1000px" }}
+        // onMouseEnter={() => setResetRotation(false)}
+        // onMouseLeave={() => setResetRotation(true)}
       >
-        <ProductView />
+        <ProductView reset={false} />
       </Canvas>
     </div>
   );
