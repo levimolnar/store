e646d602dd059d34a97ec93d5fc065fa87daca3d
levi <levimolnardev@gmail.com>
2024-04-20 03:38:28 +0200

    Added basic products slider.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,21 @@ import { useSpring, a } from '@react-spring/three'
 import "./App.css";
 import { useEffect, useRef, useState } from "react";
 
-const ProductView = ({reset, loaded}: {reset: boolean, loaded: boolean}) => {
+const ProductDescription = () => {
+  return (
+  <div className="product__description">
+    <div className="xw l condensed">SONY</div>
+    <div className="l condensed w">
+      BETACAM <span className="t">1982</span>
+    </div>
+    <div className="t s">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+    </div>
+  </div>
+  );
+};
+
+const ProductMesh = ({reset}: {reset: boolean}) => {
 
   const ref = useRef<Mesh>(null!);
   
@@ -44,19 +58,23 @@ const ProductView = ({reset, loaded}: {reset: boolean, loaded: boolean}) => {
     </a.mesh>
   );
 };
+const ProductView = () => {
+  const [resetRotation, setResetRotation] = useState(true);
 
-const ProductInfo = () => {
   return (
-  <div className="productinfo">
-    <div className="w1 size2 condensed">SONY</div>
-    <div>
-      <span className="w2 size2 condensed">BETACAM </span> 
-      <span className="w3 size2 condensed">1982</span>
+    <div className="product">
+      <ProductDescription />
+      <Canvas 
+        className="product__canvas"
+        camera={{ fov: 50 }}
+        onMouseEnter={() => setResetRotation(false)}
+        onMouseLeave={() => setResetRotation(true)}
+      >
+        <ProductMesh 
+          reset={resetRotation} 
+        />
+      </Canvas>
     </div>
-    <div className="w3 size4">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-    </div>
-  </div>
   );
 };
 
@@ -64,13 +82,28 @@ const ProductCard = () => {
   return (
     <div className="card">
       <div className="card__view">
+        <Canvas
+          camera={{ fov: 80 }}
+        >
+          <mesh 
+            scale={3} 
+            position={[0, 0, -2.5]}
+          >
+            {/* <ambientLight intensity={.1} /> */}
+            <pointLight position={[10, 10, 20]} intensity={5000} color="white" />
+            <sphereGeometry />
+            <meshStandardMaterial color="white" />
+          </mesh>
+        </Canvas>
       </div>
       <div className="card__content">
         <div className="card__column">
-          <div className="w1 size3">BETACAM</div>
-          <div className="w4 size4">⚪︎⚪︎</div>
+          <div className="xw m condensed">BETACAM</div>
+          <div className="xt m">⚪︎⚪︎⚪︎⚪︎⚪︎</div>
         </div>
-        <div className="card__column w4 size3">€1299.99</div>
+        <div className="card__column t s">
+          <div>€<span className="m">1299</span>.99</div>
+        </div>
       </div>
     </div>
   );
@@ -83,6 +116,7 @@ const Carousel = () => {
       <ProductCard />
       <ProductCard />
       <ProductCard />
+      <ProductCard />
     </div>
   );
 };
@@ -90,24 +124,13 @@ const Carousel = () => {
 
 
 const App = () => {
-
-  const [resetRotation, setResetRotation] = useState(true);
-
   return (
     <div className="wrap">
-      <div className="label size1 condensed">
-        <span className="w1">PRODUCT</span>&nbsp;
-        <span className="w3">SHOWCASE</span>
+      <div className="page__title xl condensed">
+        <span className="xw">PRODUCT</span>&nbsp;
+        <span className="t">SHOWCASE</span>
       </div>
-      <ProductInfo />
-      <Canvas 
-        camera={{ fov: 50 }}
-        style={{ width: "1000px", height: "1000px", borderRadius: "50%", zIndex: "2"}} // , background: "radial-gradient(transparent, black)"
-        onMouseEnter={() => setResetRotation(false)}
-        onMouseLeave={() => setResetRotation(true)}
-      >
-        <ProductView reset={resetRotation} loaded={true}/>
-      </Canvas>
+      <ProductView />
       <Carousel />
     </div>
   );
